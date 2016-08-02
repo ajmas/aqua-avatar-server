a16512f6184232d86b93f4bb9e4cd2a41b4c95f2
Andre Mas <andrejohn.mas@gmail.com>
2016-08-02 14:46:34 -0400

    Updated package and application
    In preparing to make this available via NPM, I have update the
    package.json and also updated the code to deal with multiple
    requests and better handle certain exceptions.
    
    This is still an early version, so there may still be other
    issues. Oh, and we really need to add some tests.

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Aqua Avatar Server
 ==================
 
-A standalone nodejs based web server to store avatar images and serve them up at the requested scale. If a correspoding avatar is not found, then a default avatar is served up. If the browser supports webp, then the image is served up as webp, otherwise it is served up as jpg.
+A simple standalone, Node.js based, web server to store avatar images and serve them up at the requested scale. If a correspoding avatar is not found, then a default avatar is served up. If the browser supports webp, then the image is served up as webp, otherwise it is served up as jpg.
+
+The image scaling capability is delegated to Imagemagick, via the imagemagick package.
+
+The REST interface was inspired by the one used by GitHub's avatar server, mainly as a way to be easily used by the client tools already written with GitHub's avatar server in mind.
 
 Currently there is no security around uploads, so it will either need to be implemented or the upload path protected from external requests.
 
-Contributions welcome
+Contributions and suggestions are welcome.
 
 Current usage:
 
@@ -23,5 +27,11 @@ Examples
 
   - Read, at 256px: ```http://localhost:3011/u/12345?s=256```
   - Write, with curl: ``` curl -i -F filedata=@myavatar.jpg http://localhost:3011/u/12345```
+
+License
+-------
+
+Licensed under the GPL v3. For details please consult the LICENSE file.
+
 
 
--- app.js
+++ app.js
@@ -1,104 +1,195 @@
 'use strict'
 
+const version = '0.5.0';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const os = require('os');
 const fileUpload = require('express-fileupload');
-
 const imagemagick = require('imagemagick');
-const imagemagickStream = require('imagemagick-stream');
+const shortid = require('shortid');
 
-const settings = {
+const defaultSettings = {
     listeningPort: 3011,
+    tmpDir: os.tmpdir(),
     defaultImagePath: __dirname + '/' + 'data/default-image.jpg',
     originalsDirectory: __dirname + '/' + 'data/originals',
     basePath: '/u/',
-    writeKey: '12432f23s31'
+    writeKey: '12432f23s31',
+    // default 'width', but also used for the height
+    defaultImageWidth: 32,
+    // list of mime-types, used to override default suffix mapping
+    mimeTypes: {
+        'apng': 'image/png'
+    },
+    cache: {
+        maxAgeMs: 60 * 1000
+    }
 };
 
+class AquaAvatarServer {
 
-
-function fileExists(filePath) {
-    try {
-        return fs.statSync(filePath).isFile();
-    } catch (e) {
-        console.log(e);
-        return false;
+    /**
+     * Constructs the avatar server. Values in passed settings
+     * will be used to override default settings. Anything not
+     * specified will come fromt the default settings.
+     */
+    constructor(settings) {
+        this.settings = defaultSettings;
+        if (settings) {
+            Object.assign(this.settings, settings);
+        }
     }
+
+    /**
+     * Tests to see if the file exists. Synchronous operation.
+     */
+    fileExists(filePath) {
+        try {
+            return fs.statSync(filePath).isFile();
+        } catch (e) {
+            return false;
+        }
+    }
+
+    registerHandlers(app) {
+
+        // Returns an image for the specified avatar id, defaulting to the
+        app.get(/\/(.+)/, function (req, res) {
+            const avatarId = req.params[0];
+
+            // Get the requested image size and
+            let size = req.query['s'];
+            if (size === undefined) {
+                size = this.settings.defaultImageWidth;
+            } else if (isNaN(size)) {
+                size = this.settings.defaultImageWidth;
+            }
+            size = size + 'x' + size;
+
+            // We default to jpg, but if the browser specifically indicates it supports
+            // 'webp', then we will provide the image in that format.
+            var imageType = 'jpg';
+            if (req.get('accept').indexOf('image/webp') > -1) {
+                imageType = 'webp';
+            }
+
+            // Allow image type override, though limited to a specific set
+            // that we have tested
+            if (req.query['t']) {
+                if (req.query['t'] === 'jpg') {
+                    imageType = 'jpg';
+                } else if (req.query['t'] === 'gif') {
+                    imageType = 'gif';
+                } else if (req.query['t'] === 'apng') {
+                    imageType = 'apng';
+                }
+            }
+
+            // Provide a means to override the response mimetype. Introduced to deal
+            // with the fact 'apng's were defaulting to application/octet-stream
+            if (this.settings.mimeTypes && this.settings.mimeTypes[imageType]) {
+                console.log('def');
+                res.contentType(this.settings.mimeTypes[imageType]);
+            }
+
+            let avatarPath = this.settings.originalsDirectory + '/' + avatarId + '.dat';
+
+            if (!this.fileExists(avatarPath)) {
+                avatarPath = this.settings.defaultImagePath;
+            }
+
+            const tmpName = shortid.generate() + '.' + imageType;
+            const outPath = this.settings.tmpDir + '/' + tmpName;
+
+            console.log('outPath', outPath);
+            imagemagick.convert([avatarPath, '-resize', size, outPath], function (err, stdout) {
+                if (err) {
+                    throw err;
+                }
+
+                // set the value for the maxAge header, default to zero if none specified
+                var maxAge = 0;
+                if (this.settings.cache && this.settings.cache.maxAgeMs) {
+                    maxAge = this.settings.cache.maxAgeMs;
+                }
+
+                if (this.fileExists(outPath)) {
+                    // send the converted file and remove it once done
+                    res.sendFile(outPath, { maxAge: maxAge }, function (err) {
+                        try {
+                            fs.unlinkSync(outPath);
+                        } catch (e) {
+                            // ignore error
+                        }
+
+                    });
+                } else {
+                    res.status('415','unsupported media type');
+                    res.write('415 - unsupported media type');
+                    res.end();
+                }
+            }.bind(this));
+
+        }.bind(this));
+
+        // Uploads the image
+        app.post(/\/(.+)/, function (req, res) {
+            const avatarId = req.params[0];
+            if (!req.files) {
+                res.send('No files were uploaded.');
+                return;
+            }
+
+            let avatarFile = req.files.filedata;
+            console.log('files uploaded: ', req.files.filedata);
+            avatarFile.mv(this.settings.originalsDirectory + '/' + avatarId + '.dat', function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.send('File uploaded!');
+                }
+            });
+        }.bind(this));
+     
+    }
+
+    start() {
+        let app = express();
+
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+
+        app.use(bodyParser.json());
+
+        app.use(fileUpload());
+
+        app.get(/\//, function (req, res) {
+            res.write('Aqua Avatar Server ' + version);
+            res.end();
+        });   
+
+        var router = express.Router();
+
+        app.use(this.settings.basePath, router);
+
+        this.registerHandlers(router);
+
+        app.listen(this.settings.listeningPort);
+    }
+
 }
 
-// TODO see: http://blog.ragingflame.co.za/2015/2/6/resizing-images-in-nodejs-applications
+// If we are being called directly then assume we are being
+// invoked from the command line, otherwise export the class.
 
-function registerHandlers(app) {
-    // Scales and returns the images
-    app.get(/\/(.+)/, function (req, res) {
-        const avatarId = req.params[0];
-
-        let size = req.query['s'];
-        if (size === undefined) {
-            size = 32;
-        } else if (isNaN(size)) {
-            size = 32;
-        }
-        size = size + 'x' + size;
-
-        var imageType = 'jpg';
-        if (req.get('accept').indexOf('image/webp') > -1) {
-           console.log('accepts webp');
-           imageType = 'webp';
-        }
-
-        if (req.query['t'] && req.query['t'] === 'jpg') {
-            imageType = 'jpg';
-        }
-
-
-        let avatarPath = settings.originalsDirectory + '/' + avatarId + '.jpg';
-
-        if (!fileExists(avatarPath)) {
-            avatarPath = settings.defaultImagePath;
-        }
-
-        let outPath = __dirname + '/' + 'out.' + imageType;
-        imagemagick.convert([avatarPath, '-resize', size, outPath],
-        function(err, stdout){
-          if (err) {throw err;}
-          console.log('stdout:', stdout);
-          res.sendFile(outPath);
-          // TODO erase temporary file
-        });
-    });
-
-    // Uploads the image
-    app.post(/\/(.+)/, function (req, res) {
-        const avatarId = req.params[0];
-        if (!req.files) {
-            res.send('No files were uploaded.');
-            return;
-        }
-
-        let avatarFile = req.files.filedata;
-        console.log('files uploaded: ', req.files.filedata);
-        avatarFile.mv(settings.originalsDirectory + '/' + avatarId + '.jpg', function(err) {
-        if (err) {
-            res.status(500).send(err);
-        }
-        else {
-            res.send('File uploaded!');
-        }
-    });
-    });
+if (module.filename === process.mainModule.filename) {
+    new AquaAvatarServer().start();
+} else {
+    module.exports = AquaAvatarServer;
 }
 
-let app = express();
 
-app.use(bodyParser());
-app.use(fileUpload());
-
-var router = express.Router();
-
-app.use(settings.basePath, router);
-
-registerHandlers(router);
-
-app.listen(settings.listeningPort);
